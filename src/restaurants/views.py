4de29eb08115f983ac674b2e0059d87c8fe5c93c
Pawel Kaminski <kaminskipawel@hotmail.com>
2019-02-10 22:01:35 +0100

    Implement section 3

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,31 +1,41 @@
-import random
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.db.models import Q
 
-class HomeView(TemplateView):
-    template_name = 'home.html'
+from .models import RestaurantLocation
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(HomeView, self).get_context_data(*args, **kwargs)
-        num = None
-        some_list = [
-            random.randint(0, 100000000),
-            random.randint(0, 100000000),
-            random.randint(0, 100000000)
-        ]
-        condition_bool_item = True
-        if condition_bool_item:
-            num = random.randint(0, 100000000)
-        context = {
-            "num": num,
-            "some_list": some_list
-        }
-        return context
+def restaurant_listview(request):
+    template_name = 'restaurants/restaurants_list.html'
+    queryset = RestaurantLocation.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, template_name, context)
 
-# class AboutView(TemplateView):
-#     template_name = "about.html"
-#
-# class ContactView(TemplateView):
-#     template_name = "contact.html"
+class RestaurantListView(ListView):
+    def get_queryset(self):
+        #self.kwargs = {'slug': 'to-co-wpisałeś-do-browsera'}
+        slug = self.kwargs.get('slug')
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                    Q(category__iexact=slug) |
+                    Q(category__icontains=slug)
+            )
+        else:
+            queryset = RestaurantLocation.objects.all()
+        return queryset
+
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all()
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
+    # def get_object(self, *args, **kwargs):
+    #     rest_id = self.kwargs.get('rest_id')
+    #     obj = get_object_or_404(RestaurantLocation, id=rest_id)
+    #     return obj
