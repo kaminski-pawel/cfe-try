05cfbaa56fe4eb5721c63b6c53483f1b07c8343a
Pawel Kaminski <kaminskipawel@hotmail.com>
2019-02-11 18:45:49 +0100

    Implement ckecklist 22-27

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView
 from django.db.models import Q
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 
+from .forms import RestaurantCreateForm, RestaurantLocationCreateForm
 from .models import RestaurantLocation
 
-def restaurant_listview(request):
+
+def restaurant_createview(request):
+    form = RestaurantLocationCreateForm(request.POST or None)
+    errors = None
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/restaurants/")
+    if form.errors:
+        errors = form.errors
+
+    template_name = 'restaurants/form.html'
+    context = {"form": form, "errors": errors}
+    return render(request, template_name, context)
+
+
+
+def restaurant_listview(request,):
     template_name = 'restaurants/restaurants_list.html'
     queryset = RestaurantLocation.objects.all()
     context = {
@@ -14,28 +31,38 @@ def restaurant_listview(request):
     }
     return render(request, template_name, context)
 
+
+def restaurant_detailview(request, slug):
+    template_name = 'restaurants/restaurantlocation_detail.html'
+    obj = RestaurantLocation.objects.get(slug=slug)
+    context = {
+        "object": obj
+    }
+    return render(request, template_name, context)
+
 class RestaurantListView(ListView):
     def get_queryset(self):
-        #self.kwargs = {'slug': 'to-co-wpisałeś-do-browsera'}
-        slug = self.kwargs.get('slug')
+        slug = self.kwargs.get("slug")
         if slug:
             queryset = RestaurantLocation.objects.filter(
                     Q(category__iexact=slug) |
                     Q(category__icontains=slug)
-            )
+                )
         else:
             queryset = RestaurantLocation.objects.all()
         return queryset
 
-class RestaurantDetailView(DetailView):
-    queryset = RestaurantLocation.objects.all()
 
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
-    #     print(context)
-    #     return context
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all() #.filter(category__iexact='asian') # fitler by user
 
     # def get_object(self, *args, **kwargs):
     #     rest_id = self.kwargs.get('rest_id')
-    #     obj = get_object_or_404(RestaurantLocation, id=rest_id)
+    #     obj = get_object_or_404(RestaurantLocation, id=rest_id) # pk = rest_id
     #     return obj
+
+
+class RestaurantCreateView(CreateView):
+    form_class = RestaurantLocationCreateForm
+    template_name = 'restaurants/form.html'
+    success_url = "/restaurants/"
