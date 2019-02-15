4bf7c80b458f9d6735c55695c56d76d56a2d8362
Pawel Kaminski <kaminskipawel@hotmail.com>
2019-02-15 01:12:38 +0100

    Implement checklist 38-40

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
 from django.conf import settings
 from django.core.urlresolvers import reverse
@@ -8,7 +9,28 @@ from .validators import validate_category
 
 User = settings.AUTH_USER_MODEL
 
-# Create your models here.
+class RestaurantLocationQuerySet(models.query.QuerySet):
+    def search(self, query): #RestaurantLocation.objects.all().search(query) #RestaurantLocation.objects.filter(something).search()
+        if query:
+            query = query.strip()
+            return self.filter(
+                Q(name__icontains=query)|
+                Q(location__icontains=query)|
+                Q(location__iexact=query)|
+                Q(category__icontains=query)|
+                Q(category__iexact=query)|
+                Q(item__name__icontains=query)|
+                Q(item__contents__icontains=query)
+                ).distinct()
+        return self
+
+class RestaurantLocationManager(models.Manager):
+    def get_queryset(self):
+        return RestaurantLocationQuerySet(self.model, using=self._db)
+
+    def search(self, query):
+        return self.get_queryset().search(query)
+
 class RestaurantLocation(models.Model):
     owner           = models.ForeignKey(User)
     name            = models.CharField(max_length=120)
@@ -17,6 +39,7 @@ class RestaurantLocation(models.Model):
     timestamp       = models.DateTimeField(auto_now_add=True)
     updated         = models.DateTimeField(auto_now=True)
     slug            = models.SlugField(null=True, blank=True)
+    objects         = RestaurantLocationManager()
 
     def __str__(self):
         return self.name
